f3be9f9185b45fc3faf96e3993bdd0aac6a59cba
Rahul Prasad <rahulprasad2823@gmail.com>
2024-01-26 21:19:26 -0500

    Create Classwork1.js

---
+++ Classwork1.js
@@ -0,0 +1,17 @@
+//Collection and Document Creation with an Employee Details
+db.createCollection('Details')
+db.Details.insertOne({
+    "_id": 123,
+    "Emp_ID": "10025AE336",
+    "Personal_details": {
+        "First_Name": "Radhika",
+        "Last_Name": "Sharma",
+        "Date_Of_Birth": "1995-09-26"
+    },
+    "Contact": {
+        "e-mail": "radhika_sharma.123@gmail.com",
+        "phone": "9848022338"
+    },
+    "Address": {
+        "city": "Hyderabad",
+        "Area": "Madapur",}})
