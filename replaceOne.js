2e345b883c7f97231232fe4a11cfb33067f89d8b
Rahul Prasad <rahulprasad2823@gmail.com>
2024-02-08 14:39:37 -0500

    replaceOne.js
     The code uses "replaceOne" to update a document in the "books" collection by replacing its content based on the specified '_id' and providing updated field values.

---
+++ replaceOne.js
@@ -0,0 +1,33 @@
+//replaceOne()
+db.books.replaceOne(
+  {_id: ObjectId("65c292b7dd77e720d30e243e")},
+  {
+    _id: ObjectId("65c292b7dd77e720d30e243e"),
+    title: "Deep Dive into React Hooks",
+    ISBN: "0-3182-1299-4",
+    thumbnailUrl: "http://via.placeholder.com/640x360",
+    publicationDate: ISODate("2022-07-28T02:20:21.000Z"),
+    authors: ["Ada Lovelace"],
+  }
+);
+
+db.books.replaceOne(
+  {_id: ObjectId("65c292b7dd77e720d30e243e")},
+  {
+    title: "Deep Dive into React Hooks",
+    ISBN: "0-3182-1299-6",
+    thumbnailUrl: "http://via.placeholder.com/640x360",
+    publicationDate: ISODate("2022-07-28T02:20:21.000Z"),
+    authors: ["Ada Lovelace"],
+  }
+);
+
+/* Output:
+{
+  acknowledged: true,
+  insertedId: null,
+  matchedCount: 0,
+  modifiedCount: 0,
+  upsertedCount: 0
+}
+*/
